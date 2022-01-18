1580dcf2dae71b60d54fb0c71fe99edb9d4ae348
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2022-01-18 00:24:56 -0300

    Rectangle Exercise solved

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,6 +11,7 @@ namespace Program
 
             Console.WriteLine("Por favor, ingresa el largo del rectángulo que deseas imprimir en consola: ");
             int lengthEntered = Convert.ToInt32(Console.ReadLine());
+            
             RectangleExercise rectangle1 = new RectangleExercise(widthEntered, lengthEntered);
 
             Console.WriteLine($"Ok, vamos a dibujar el rectángulo: \n{rectangle1.PrintRectangle()}");          
