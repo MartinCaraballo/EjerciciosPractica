8fcd7f6baaafbdf1c3df5c2ba02ccbab5085c5a1
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2022-01-18 00:21:24 -0300

    Rectangle Exercise solved

--- src/Library/RectangleExercise.cs
+++ src/Library/RectangleExercise.cs
@@ -1,12 +1,42 @@
-﻿namespace Library;
+﻿using System.Text;
+
+namespace Exercises;
+
+/// <summary>
+/// This class represents an rectangle, that can be printed in console.
+/// </summary>
 public class RectangleExercise
 {
-    public int Wide { get; private set; }
-    public int Large { get; private set; }
+    public int Width { get; private set; }
+    public int Length { get; private set; }
 
-    public RectangleExercise(int wide, int large)
+    /// <summary>
+    /// Initializes an instance of <see cref="RectangleExercise"/> class.
+    /// </summary>
+    /// <param name="width">Rectangle's wide.</param>
+    /// <param name="length">Rectangle's large.</param>
+    public RectangleExercise(int width, int length)
     {
-        this.Wide = wide;
-        this.Large = large;
+        this.Width = width;
+        this.Length = length;
+    }
+
+    public string PrintRectangle()
+    {
+        StringBuilder resultToBePrinted = new StringBuilder();
+
+        // Draw the top length of the rectangle.
+        resultToBePrinted.Append('X', this.Width).AppendLine();
+
+        // Draw the mid-part of the rectangle.
+        for (int length = 0; length < this.Length; length++)
+        {
+            resultToBePrinted.Append("x").Append(' ', this.Length - 2).Append("x").AppendLine();
+        }
+
+        // Draw the bottom length of the rectangle.
+        resultToBePrinted.Append('X', this.Width);
+
+        return resultToBePrinted.ToString();
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Program
+﻿using Exercises;
+
+namespace Program
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Por favor, ingresa el ancho del rectángulo que deseas imprimir en consola: ");
+            int wideEntered = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Por favor, ingresa el largo del rectángulo que deseas imprimir en consola: ");
+            int largeEntered = Convert.ToInt32(Console.ReadLine());
+            RectangleExercise rectangle1 = new RectangleExercise(wideEntered, largeEntered);
+            Console.WriteLine($"Ok, vamos a dibujar el rectángulo: \n{rectangle1.PrintRectangle()}");          
         }
     }
 }
