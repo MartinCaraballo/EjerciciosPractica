8b2369c13c53f79ce94d71b0e837ac963bdeb5fc
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2022-01-17 23:27:51 -0300

    Rectangle Exercise

---
+++ src/Library/RectangleExercise.cs
@@ -0,0 +1,12 @@
+﻿namespace Library;
+public class RectangleExercise
+{
+    public int Wide { get; private set; }
+    public int Large { get; private set; }
+
+    public RectangleExercise(int wide, int large)
+    {
+        this.Wide = wide;
+        this.Large = large;
+    }
+}
