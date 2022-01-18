a8f786701cfa81c5da0b6496f8b4114b0b2e1d13
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2022-01-18 00:23:39 -0300

    Rectangle Exercise solved

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,11 +6,14 @@ namespace Program
     {
         public static void Main(string[] args)
         {
+            // Request the width to the user.
             Console.WriteLine("Por favor, ingresa el ancho del rectángulo que deseas imprimir en consola: ");
-            int wideEntered = Convert.ToInt32(Console.ReadLine());
+            int widthEntered = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Por favor, ingresa el largo del rectángulo que deseas imprimir en consola: ");
-            int largeEntered = Convert.ToInt32(Console.ReadLine());
-            RectangleExercise rectangle1 = new RectangleExercise(wideEntered, largeEntered);
+            int lengthEntered = Convert.ToInt32(Console.ReadLine());
+            RectangleExercise rectangle1 = new RectangleExercise(widthEntered, lengthEntered);
+
             Console.WriteLine($"Ok, vamos a dibujar el rectángulo: \n{rectangle1.PrintRectangle()}");          
         }
     }
